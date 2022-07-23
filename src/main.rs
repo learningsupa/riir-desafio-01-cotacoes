fca3449df6607ef996bf78b8ea17ec6265e9afbb
learningsupa <learningissupaeasy@outlook.com>
2022-07-23 11:49:26 -0300

    feat: create body and query struct to handlers
    - add serde/serde_json deps
    - fix http method to service c

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
-use axum::routing::get;
-use axum::Router;
+use axum::extract::Query;
+use axum::routing::{get, post};
+use axum::{Json, Router};
 use hyper::Server;
+use serde::Deserialize;
 
-async fn service_a() -> &'static str {
+#[derive(Deserialize)]
+struct QueryServiceA {
+    curr: String
+}
+
+async fn service_a(Query(_query): Query<QueryServiceA>) -> &'static str {
     "serviço a"
 }
 
-async fn service_b() -> &'static str {
+#[derive(Deserialize)]
+struct QueryServiceB {
+    moeda: String
+}
+
+async fn service_b(Query(_query): Query<QueryServiceB>) -> &'static str {
     "serviço b"
 }
 
-async fn service_c() -> &'static str {
-    "serviço b"
+#[derive(Deserialize)]
+struct BodyServiceC {
+    callback: String,
+    tipo: String
+}
+
+async fn service_c(Json(_payload): Json<BodyServiceC>) -> &'static str {
+    "serviço c"
 }
 
 #[tokio::main]
@@ -19,7 +37,7 @@ async fn main() -> anyhow::Result<()> {
     let app = Router::new()
         .route("/servico-a/cotacao", get(service_a))
         .route("/servico-b/cotacao", get(service_b))
-        .route("/servico-c/cotacao", get(service_c));
+        .route("/servico-c/cotacao", post(service_c));
 
     Server::bind(&"0.0.0.0:3000".parse()?)
         .serve(app.into_make_service())
